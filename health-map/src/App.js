635edddde133b334d086962b4c1283c4b3a0f8d3
Utsab Upadhyay <utsab@Utsabs-MacBook-Pro.local>
2017-09-16 20:40:45 -0500

    the fields added

--- health-map/src/App.js
+++ health-map/src/App.js
@@ -3,25 +3,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
+import InputSlider from 'react-input-slider';
 
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: 100.397, lng: 250.644 };
-  }
+    this.state = { lat: 100.397, lng: 250.644, x: 10, y: 10 };
+}
 
 
   render() {
 
     return (
-        <div style = {{height: '100%' }}>
-        
+        <div style = {{height: '100%' }}  >
         </div>
 
-    );
-  }
+      );
+    }
+
+    handleChange = pos => {
+      this.setState ({
+        x: pos.x,
+        y: pos.y
+      });
+    };
+
 }
